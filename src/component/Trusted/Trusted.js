1383ff3e577cd7e5fedb89877aa84f1595a4373e
sadik126 <sadikhimel04@gmail.com>
2022-11-30 06:14:30 +0600

    m

--- src/component/Trusted/Trusted.js
+++ src/component/Trusted/Trusted.js
@@ -3,6 +3,25 @@ import React from 'react';
 const Trusted = () => {
     return (
         <div>
+            <h4>Trusted by 100+ companies</h4>
+            <div className="d-flex justify-content-center align-items-center">
+                <div>
+                    <img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png" alt="" />
+                </div>
+                <div>
+                    <img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png" alt="" />
+                </div>
+                <div>
+                    <img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png" alt="" />
+                </div>
+                <div>
+                    <img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png" alt="" />
+                </div>
+                <div>
+                    <img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png" alt="" />
+                </div>
+
+            </div>
 
         </div>
     );
