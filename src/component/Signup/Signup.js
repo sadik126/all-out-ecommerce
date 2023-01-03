8b82b0e44c8630df6927d0e2029a0ec93190a1d9
sadik126 <sadikhimel04@gmail.com>
2023-01-03 20:06:01 +0600

    m

--- src/component/Signup/Signup.js
+++ src/component/Signup/Signup.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
+// import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from '../../firebase.init';
 
 const Signup = () => {
-    const { email, setEmail } = useState('');
-    const { password, setPassword } = useState('');
-    const { name, setName } = useState('');
-    const { confirmpassword, setConfirmpassword } = useState('');
-    const { error, setError } = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [confirmpassword, setConfirmpassword] = useState('');
+    const [error, setError] = useState('');
+
+    const auth = getAuth(app);
+
+    // const [
+    //     createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
 
 
     const handleemailBlur = event => {
@@ -27,6 +36,33 @@ const Signup = () => {
         setConfirmpassword(event.target.value)
     }
 
+    const createUser = event => {
+        event.preventDefault();
+        console.log(email, password)
+        if (password !== confirmpassword) {
+            setError('your password didnt match ')
+            return;
+        }
+
+        if (password.length < 6) {
+            setError('password must be 6 charecter or longer')
+            return
+        }
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setEmail('')
+                setPassword('')
+                setConfirmpassword('')
+                setName('')
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
 
     return (
 
@@ -35,8 +71,9 @@ const Signup = () => {
             <div className='login-container p-5'>
 
 
-                <form>
+                <form onSubmit={createUser}>
                     <h1 className='fw-bolder fs-2 title'>SIGNUP</h1>
+                    <p className='text-danger text-center'>{error}</p>
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
                         <input onBlur={handlenameBlur} type="text" class="form-control" id="name" />
@@ -52,7 +89,7 @@ const Signup = () => {
                     </div>
                     <div class="mb-3">
                         <label for="confirmpassword" class="form-label">Confirm Password</label>
-                        <input onBlur={handlecpasswordBlur} type="cpassword" class="form-control" id="confirmpassword" />
+                        <input onBlur={handlecpasswordBlur} type="password" class="form-control" id="confirmpassword" />
                     </div>
                     <div class="mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1" />
